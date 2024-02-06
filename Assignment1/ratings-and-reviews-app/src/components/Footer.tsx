a9bfd3d8f6950e1a3d333e53307b7f5b6390b945
Aditya Maheshbhai Purohit <aditya.purohit@dal.ca>
2024-02-06 14:50:12 -0400

    modify A-1 footer

--- Assignment1/ratings-and-reviews-app/src/components/Footer.tsx
+++ Assignment1/ratings-and-reviews-app/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 // Footer Component of the webpage.
 export function Footer() {
   return (
-    <footer className="bg-sky-700 rounded-lg shadow mt-2">
+    <footer className="bg-sky-700 shadow mt-2">
       <div className="w-full mx-auto max-w-screen p-4 flex flex-col md:flex-row items-center justify-between">
         <span className="text-sm text-white sm:text-center ">
           © 2024{" "}
